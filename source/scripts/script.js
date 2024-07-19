ef55bb1084c4baa2a3510c3164a4edd6531827e1
mohammad-tarah <aqatarahomi8@gmail.com>
2024-07-19 20:54:33 +0330

    ok

---
+++ source/scripts/script.js
@@ -0,0 +1,35 @@
+function navscroll()
+{
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.querySelector("nav").classList.add("nav-background");
+    }
+    else{
+        document.querySelector("nav").classList.remove("nav-background");
+    }
+
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("back_to_top").style.visibility="visible";
+    }
+    else{
+        document.getElementById("back_to_top").style.visibility="hidden";
+    }
+}
+
+let navmenulinks = document.querySelectorAll('.navbar a');
+
+function navmenuScrollspy() {
+  navmenulinks.forEach(navmenulink => {
+    if (!navmenulink.hash) return;
+    let section = document.querySelector(navmenulink.hash);
+    if (!section) return;
+    let position = window.scrollY + 200;
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
+      navmenulink.classList.add('active');
+    } else {
+      navmenulink.classList.remove('active');
+    }
+  })
+}
+window.addEventListener('load', navmenuScrollspy);
+document.addEventListener('scroll', navmenuScrollspy);
