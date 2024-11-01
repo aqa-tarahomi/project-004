c0b68f730de6e039ab338d12c34683964289bb0b
mohammad-tarah <aqatarahomi8@gmail.com>
2024-11-01 19:27:03 +0330

    ok

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -3,5 +3,32 @@ function navscroll() {
  }
  AOS.init();
 
+ const swiperEl = document.querySelector('swiper-container')
+    Object.assign(swiperEl, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        clickable: true,
+      },
+      breakpoints: {
+        1: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          992: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1200: {
+            slidesPerView: 6,
+            spaceBetween:100,
+          },
+      },
+    });
+    swiperEl.initialize();
 
         
