2cb2b274f3eb1859be885837c51325f74054cf30
mohammad-tarah <aqatarahomi8@gmail.com>
2024-11-22 01:38:13 +0330

    ok

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -35,62 +35,10 @@ function navscroll() {
 
  AOS.init();
 
- const swiperEl = document.querySelector('swiper-container')
-    Object.assign(swiperEl, {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      pagination: {
-        clickable: true,
-      },
-      breakpoints: {
-        1: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-          992: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1200: {
-            slidesPerView: 5,
-            spaceBetween:100,
-          },
-      },
-    });
-    swiperEl.initialize();
-
-    // Select the elements to animate
-const animatedElements = document.querySelectorAll('.card-text');
-
-// Function to check if element is in the viewport
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.bottom >= 0
-  );
-}
-
-// Function to activate the animation
-function activateAnimation() {
-  animatedElements.forEach((element) => {
-    if (isInViewport(element)) {
-      element.classList.add('animate');
-    }
-  });
-}
-
-// Listen for scroll events and activate animation on scroll
-window.addEventListener('scroll', activateAnimation);
-
-// Optionally, call the function once to activate animations if elements are in view on load
-activateAnimation();
+ 
 
 document.getElementById("back_to_top").addEventListener("click", function (e) {
   e.preventDefault();
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
