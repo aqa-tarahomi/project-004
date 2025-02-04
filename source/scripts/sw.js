3c0dc0963ddb0b67e26c6344c357448a418aec2e
mohammad-tarah <aqatarahomi8@gmail.com>
2025-02-05 00:10:26 +0330

    ok

--- source/scripts/sw.js
+++ source/scripts/sw.js
@@ -3,7 +3,7 @@ const ASSETS = [
     '/',
   
     // CSS files
-    '../styles/style.css',
+    'source/styles/style.css',
     '../bootstrap-5.3.2-dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
     'https://unpkg.com/aos@2.3.1/dist/aos.css',
@@ -25,35 +25,18 @@ const ASSETS = [
     'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap',
 ];
 
-// Install the service worker and cache assets
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+      caches.open(CACHE_NAME).then((cache) => {
+          return cache.addAll(FILES_TO_CACHE);
+      })
   );
 });
 
-// Fetch cached assets when offline
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+      })
   );
 });
