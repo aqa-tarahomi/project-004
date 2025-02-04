d90dd425f64e6a3b7a8aa2f5aad5dd34d2fb5685
mohammad-tarah <aqatarahomi8@gmail.com>
2025-02-04 20:43:53 +0330

    ok

--- source/scripts/sw.js
+++ source/scripts/sw.js
@@ -1,15 +1,31 @@
 const CACHE_NAME = 'isitschool-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/source/styles/style.css',
-  '/source/bootstrap-5.3.2-dist/css/bootstrap.min.css',
-  '/source/gallery/logo.png',
-  '/source/scripts/script.js',
-  '/source/scripts/swiper.js',
-  'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
-  'https://unpkg.com/aos@2.3.1/dist/aos.css',
-  'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap'
+    '/',
+    '/index.html',
+    '/main/Portfolio/index.html',
+    '/main/contact-us/index.html',
+  
+    // CSS files
+    '/source/styles/style.css',
+    '/source/bootstrap-5.3.2-dist/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+    'https://unpkg.com/aos@2.3.1/dist/aos.css',
+  
+    // JavaScript files
+    '/source/scripts/script.js',
+    '/source/scripts/swiper.js',
+    'https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js',
+    'https://unpkg.com/aos@2.3.1/dist/aos.js',
+    'https://kit.fontawesome.com/41290f7b5b.js',
+  
+    // Images and icons
+    // '/source/gallery/logo.png',
+    '/source/gallery/test-exam.jpg',
+    '/source/gallery/anformatic_students.jpg',
+    '/source/gallery/favicon.ico',
+  
+    // Fonts
+    'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap',
 ];
 
 // Install the service worker and cache assets
