01f75131c715374d8887d10d83d1251641a5a426
mohammad-tarah <aqatarahomi8@gmail.com>
2025-02-05 00:49:28 +0330

    ok

--- source/scripts/sw.js
+++ source/scripts/sw.js
@@ -28,7 +28,7 @@ const ASSETS = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
       caches.open(CACHE_NAME).then((cache) => {
-          return cache.addAll(FILES_TO_CACHE);
+          return cache.addAll(ASSETS);
       })
   );
 });
