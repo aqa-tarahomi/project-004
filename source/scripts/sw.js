c8ac5ad0d40aa5d0d58859b592285ace9f27646c
mohammad-tarah <aqatarahomi8@gmail.com>
2025-02-05 01:26:49 +0330

    ok

--- source/scripts/sw.js
+++ source/scripts/sw.js
@@ -3,14 +3,14 @@ const ASSETS = [
     '/',
   
     // CSS files
-    'source/styles/style.css',
-    '../bootstrap-5.3.2-dist/css/bootstrap.min.css',
+    '/source/styles/style.css',
+    '/source/bootstrap-5.3.2-dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
     'https://unpkg.com/aos@2.3.1/dist/aos.css',
   
     // JavaScript files
-    '../scripts/script.js',
-    '../scripts/swiper.js',
+    '/source/scripts/script.js',
+    '/source/scripts/swiper.js',
     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js',
     'https://unpkg.com/aos@2.3.1/dist/aos.js',
     'https://kit.fontawesome.com/41290f7b5b.js',
