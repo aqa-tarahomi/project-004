20f06ecb8512f3aa8a4e6890cc7e2322bb0e111c
mohammad-tarah <aqatarahomi8@gmail.com>
2025-02-04 21:22:19 +0330

    ok

--- source/scripts/sw.js
+++ source/scripts/sw.js
@@ -3,23 +3,23 @@ const ASSETS = [
     '/',
   
     // CSS files
-    '/source/styles/style.css',
-    '/source/bootstrap-5.3.2-dist/css/bootstrap.min.css',
+    '../styles/style.css',
+    '../bootstrap-5.3.2-dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
     'https://unpkg.com/aos@2.3.1/dist/aos.css',
   
     // JavaScript files
-    '/source/scripts/script.js',
-    '/source/scripts/swiper.js',
+    '../scripts/script.js',
+    '../scripts/swiper.js',
     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js',
     'https://unpkg.com/aos@2.3.1/dist/aos.js',
     'https://kit.fontawesome.com/41290f7b5b.js',
   
     // Images and icons
-    '/source/gallery/logo.png',
-    '/source/gallery/test-exam.jpg',
-    '/source/gallery/anformatic_students.jpg',
-    '/source/gallery/favicon.ico',
+    '../gallery/logo.png',
+    '../gallery/test-exam.jpg',
+    '../gallery/anformatic_students.jpg',
+    '../gallery/favicon.ico',
   
     // Fonts
     'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap',
