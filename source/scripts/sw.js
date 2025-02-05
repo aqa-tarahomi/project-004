cfa3753f4ef2edb46dd1659ed35da8e369626482
aqa-tarahomi <152273019+aqa-tarahomi@users.noreply.github.com>
2025-02-05 10:30:33 +0330

    ok

--- source/scripts/sw.js
+++ source/scripts/sw.js
@@ -40,3 +40,12 @@ self.addEventListener("fetch", (event) => {
       })
   );
 });
+self.addEventListener("activate", (event)=>{
+    event.waitUntil(
+        caches.keys().then((cachename)=>{
+            return Promise.all(
+                cachename.map(())
+            )
+        })
+    )
+})
