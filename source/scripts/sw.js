107bb59ee4288b7ac80fa6840cca93a8b7dcef89
aqa-tarahomi <152273019+aqa-tarahomi@users.noreply.github.com>
2025-02-05 09:10:18 +0330

    Add files via upload

--- source/scripts/sw.js
+++ source/scripts/sw.js
@@ -3,23 +3,23 @@ const ASSETS = [
     '/',
   
     // CSS files
-    '/source/styles/style.css',
-    '/source/bootstrap-5.3.2-dist/css/bootstrap.min.css',
+    'project-004/source/styles/style.css',
+    'project-004/source/bootstrap-5.3.2-dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
     'https://unpkg.com/aos@2.3.1/dist/aos.css',
   
     // JavaScript files
-    '/source/scripts/script.js',
-    '/source/scripts/swiper.js',
+    'project-004/source/scripts/script.js',
+    'project-004/source/scripts/swiper.js',
     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js',
     'https://unpkg.com/aos@2.3.1/dist/aos.js',
     'https://kit.fontawesome.com/41290f7b5b.js',
   
     // Images and icons
-    '../gallery/logo.png',
-    '../gallery/test-exam.jpg',
-    '../gallery/anformatic_students.jpg',
-    '../gallery/favicon.ico',
+    'project-004/source/gallery/logo.png',
+    'project-004/source/gallery/test-exam.jpg',
+    'project-004/source/gallery/anformatic_students.jpg',
+    'project-004/source/gallery/favicon.ico',
   
     // Fonts
     'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap',
