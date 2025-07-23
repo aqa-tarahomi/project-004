42d442f43963f856504ad158212e50cb49faa0cd
mohammad-tarah <aqatarahomi8@gmail.com>
2025-07-24 02:15:45 +0330

    ok

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pwa-cache-v10"; // Increment version for debugging
+const CACHE_NAME = "pwa-cache-v9"; // Increment version for debugging
 const FILES_TO_CACHE = [
   // Local project files
   "/project-004/index.html",
